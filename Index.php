032e23f71c9f80ea072a97bd64c7160684b3db3f
asp <asp@aptech.com>
2022-03-09 18:53:36 +0500

    added index. php

---
+++ Index.php
@@ -0,0 +1,40 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <?php
+    echo "<h1>About ME</h1>";
+     echo "<h4>My name is <em>Rabia Malik</em></h4>";
+     echo "<p>I am doing Diploma in Software Engineering from Aptech </p>" ;
+     echo "<p>I glow up myself learn to avoid people who don't like me</p>";
+     print "<p>Sometimes, Iuse my imagination to make <em>myself Happy</em></p> ";
+    
+
+
+
+$name = "Rabia";
+$lname = "Malik";
+echo $name . " " .  $lname;
+
+$num1 = 4;
+$num2 = 6;
+
+$sum = $num1 + $num2;
+$sub = $num1 - $num2;
+$mul = $num1 * $num2;
+$div = $num1 / $num2;
+$mod = $num1 % $num2;
+
+echo "<br>Sum of $num1 and $num2 is ". $sum;
+echo "<br>Subtraction of $num1 and $num2 is ". $sub;
+echo "<br>Multiplication of $num1 and $num2 is ". $mul;
+echo "<br>Division of $num1 and $num2 is ". $div;
+echo "<br>Module of $num1 and $num2 is ". $mod;
+?>
+</body>
+</html>
